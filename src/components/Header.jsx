963eb4d8ad9bd4720b291bb4cb77f14fa7af00d7
Jakub Knapik <128633057+Knaappz@users.noreply.github.com>
2025-01-19 15:04:27 +0100

    color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,11 +19,11 @@ const Header = ({ onSearchChange, onSearchSubmit }) => {
   };
 
   return (
-    <section id='header' className='w-full bg-sky-100 flex flex-wrap flex-col items-center justify-center px-5 py-5 sm:py-0 sm:px-10 sm:flex-row sm:justify-between'>
+    <section id='header' className='w-full bg-sky-200 flex flex-wrap flex-col items-center justify-center px-5 py-5 sm:py-0 sm:px-32 sm:flex-row sm:justify-between'>
       <img className='w-48' src="assets/logo3.png" alt="Logo" />
-      <div id="search-wrapper" className='flex justify-evenly gap-5'>
+      <div id="search-wrapper" className='flex gap-5'>
         <input
-          className='p-2 rounded-md outline-none w-3/4 bg-sky-200 placeholder-slate-500'
+          className='p-2 rounded-md outline-none w-3/4 bg-sky-100 placeholder-slate-500'
           placeholder='Wyszukaj miasto...'
           type="text"
           value={searchTerm}
@@ -31,7 +31,7 @@ const Header = ({ onSearchChange, onSearchSubmit }) => {
           onKeyDown={handleKeyDown}
         />
         <button
-          className='bg-white p-2 rounded-md w-20 uppercase'
+          className='bg-sky-300 px-5 py-2 rounded-md uppercase'
           onClick={handleSearchClick}
         >
           Szukaj
